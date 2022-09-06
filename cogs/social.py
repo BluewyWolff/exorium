10bfb784ee954a07aae548bbd27b6d36e3a3708f
Delta <joshuaslui0203@gmail.com>
2022-09-06 21:45:14 +0200

    Update social.py

--- cogs/social.py
+++ cogs/social.py
@@ -13,7 +13,7 @@ from utils.paginator import Pages
 class social(commands.Cog, name="Social"):
     def __init__(self, bot):
         self.bot = bot
-        self.help_icon = "<:Hug:839603884716589066>"
+        self.help_icon = "♥️"
 
     @commands.command(brief="Slap someone", enabled=False)
     async def slap(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
