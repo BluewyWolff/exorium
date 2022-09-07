59543916559ace528ee7aeb58626a179156d2bea
Delta <joshuaslui0203@gmail.com>
2022-09-07 21:39:49 +0200

    Update nsfw.py

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -9,34 +9,34 @@ class nsfw(commands.Cog, name="Nsfw"):
         self.bot = bot
         self.help_icon = '🔞'
 
-        @commands.command()
-        async def e621(ctx, *, tags=''):
-            if tags.lower() in ["scat", "child"]:
-                await ctx.send("This tag is blacklisted.")
-                return
-            tagurl = tags.replace(' ', '+')
-            delmsg = await ctx.send("Waiting for results...")
-            response = requests.get(
-                f'https://e621.net/posts.json?tags={tagurl}',
-                headers={'User-Agent': config.e621agent},
-                auth=HTTPBasicAuth(config.e621username, config.e621key)
-            )
-            if not response.json()["posts"]:
-                await delmsg.delete()
-                await ctx.send(f"Sadly, we couldn't get you `{tags}`")
-                return
-            finalimg = random.choice(response.json()["posts"])["file"]["url"]
-            while True:
-                if finalimg.endswith(".webm"):
-                    finalimg = random.choice(response.json()["posts"])["file"]["url"]
-                else:
-                    break
-            embed = discord.Embed(title='Random yiff')
-            embed.set_image(url=finalimg)
-            embed.set_footer(text='Powered by e621.')
+    @commands.command()
+    async def e621(ctx, *, tags=''):
+        if tags.lower() in ["scat", "child"]:
+            await ctx.send("This tag is blacklisted.")
+            return
+        tagurl = tags.replace(' ', '+')
+        delmsg = await ctx.send("Waiting for results...")
+        response = requests.get(
+            f'https://e621.net/posts.json?tags={tagurl}',
+            headers={'User-Agent': config.e621agent},
+            auth=HTTPBasicAuth(config.e621username, config.e621key)
+        )
+        if not response.json()["posts"]:
             await delmsg.delete()
-            await ctx.send(embed=embed)
-            await functions.logging(ctx, "e621", bot)
+            await ctx.send(f"Sadly, we couldn't get you `{tags}`")
+            return
+        finalimg = random.choice(response.json()["posts"])["file"]["url"]
+        while True:
+            if finalimg.endswith(".webm"):
+                finalimg = random.choice(response.json()["posts"])["file"]["url"]
+            else:
+                break
+        embed = discord.Embed(title='Random yiff')
+        embed.set_image(url=finalimg)
+        embed.set_footer(text='Powered by e621.')
+        await delmsg.delete()
+        await ctx.send(embed=embed)
+        await functions.logging(ctx, "e621", bot)
         
 def setup(bot):
     bot.add_cog(nsfw(bot))
